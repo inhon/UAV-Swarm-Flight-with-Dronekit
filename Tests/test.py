a68a39b694988eda141805d5f532b68c6ec3d5b5
inhon <inhon@mail.cjcu.edu.tw>
2025-01-21 11:57:28 +0800

    114.01.21

--- Tests/test.py
+++ Tests/test.py
@@ -1,37 +1,21 @@
-import threading
+class MyClass:
+    def __init__(self, raw_imu):
+        self._raw_imu = raw_imu  # Private attribute
 
-class RepeatTimer(threading.Timer):
-    '''
-    Usage: 
-    timer = RepeatTimer(5, <name of function>, (args,))
-    timer.start()
-    timer.cancel()
-    '''
-    def run(self):
-        """
-        Overridden method to repeatedly call the target function at the given interval.
-        """
-        while not self.finished.wait(self.interval):  # Waits for the interval or until canceled
-            self.function(*self.args, **self.kwargs)  # Call the target function
+    @property
+    def raw_imu(self):
+        return self._raw_imu  # Getter for the private attribute
+    @raw_imu.setter
+    def raw_imu(self, value):
+        if value <0 :
+            raise ValueError("raw_imu must be non-negative")
+        self._raw_imu=value
 
-# Example function to send a message
-def sendMsg(vehicle, client):
-    print(f"Sending message to {client} with info: COORDINATES")
-    # You can replace the print with the actual functionality, e.g., vehicle.sendInfo(client, "COORDINATES")
+# Create an instance of MyClass
+my_instance = MyClass(10)
 
-# Example usage
-if __name__ == "__main__":
-    # Create a RepeatTimer to call sendMsg every 5 seconds
-    vehicle = "Car"  # Example vehicle object
-    client = "Client_123"  # Example client ID
-    timer = RepeatTimer(5, sendMsg, (vehicle, client))
-
-    # Start the timer to repeatedly call sendMsg every 5 seconds
-    timer.start()
-
-    # Run for some time and then stop
-    try:
-        # Let it run for 20 seconds before canceling
-        threading.Event().wait(20)
-    finally:
-        timer.cancel()  # Stop the timer after 20 seconds
+# Access the raw_imu property
+print(my_instance.raw_imu)  # Output: 10
+my_instance.raw_imu=20
+print(my_instance.raw_imu)
+my_instance.raw_imu = -5
